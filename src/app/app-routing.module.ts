b43815a78f283b95f06a8e1fc9b9effcc3cd4df8
AndresOreVel <121023104+AndresOreVel@users.noreply.github.com>
2025-03-04 19:11:06 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ModalComponent } from './modal/modal.component';
+import { Step1Component } from './step1/step1.component';
 
 const routes: Routes = [
   { path: 'start', component: ModalComponent},
+  { path: 'step1', component: Step1Component },
   { path: '**', redirectTo: 'start'}
 ];
 
