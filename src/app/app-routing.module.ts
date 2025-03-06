f09fe45be24deb362a8fdcac27eae7e0939db214
AndresOreVel <121023104+AndresOreVel@users.noreply.github.com>
2025-03-06 19:02:29 +0100

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ModalComponent } from './modal/modal.component';
+import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
+import { Step3Component } from './step3/step3.component';
+import { Step4Component } from './step4/step4.component';
+import { ThanyouComponent } from './thanyou/thanyou.component';
 
 const routes: Routes = [
   { path: 'start', component: ModalComponent},
+  { path: 'step1', component: Step1Component},
+  { path: 'step2', component: Step2Component},
+  { path: 'step3', component: Step3Component},
+  { path: 'step4', component: Step4Component},
+  { path: 'thankyou', component: ThanyouComponent},
   { path: '**', redirectTo: 'start'}
 ];
 
